7062fea794ab2034b50309f7c8b32c9846a3feb5
Miguel Sanchez <m1104s2003@gmail.com>
2023-03-22 21:13:58 -0400

    finished

--- .idea/coder.py
+++ .idea/coder.py
@@ -4,30 +4,33 @@ def menu():
     print('1. Encode')
     print('2. Decode')
     print('3. Quit')
-def encoder(code):
+def encoder(code1):
     string = ''
     for i in code:
         i = int(i)
         i += 3
         string += str(i)
-        return string
+
+    return string
+
 
 a = 0
 while a == 0:
     menu()
-    op = int('Please enter an option: ')
+    op = int(input('Please enter an option: '))
     if op == 3:
         break
     if op == 1:
         code = input("Please enter your password to encode: ")
-        encoder(code)
+        string1 = encoder(code)
+
         print("Your password has been encoded and stored")
-        string1 = string
+
     if op == 2:
         decode = ''
         for i in string1:
             i = int(i)
-            i = i + 3
+            i = i - 3
             decode += str(i)
         print(f'The encoded password is {string1}, and the original password is {decode}.')
 
